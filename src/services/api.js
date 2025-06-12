ed7041327a9c2decbb3bd4d8066f622762765b21
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-06-12 22:28:27 +0600

    url updated

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance with default config
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
